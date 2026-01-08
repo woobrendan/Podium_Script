84ca776a1d56f8b47f1e837e98607086bec88dbd
Brendan Woo <woo.brendan@gmail.com>
2026-01-08 14:48:07 -0800

    add eventswithfullseaon variable to handle full season as an option

--- podium-helper/src/components/EntryManager/modal/EventSelect.js
+++ podium-helper/src/components/EntryManager/modal/EventSelect.js
@@ -3,33 +3,43 @@ import { useState, useEffect } from "react";
 import { fetchEvents } from "../../../store/events/eventActions";
 
 const EventSelect = ({ entryEvent }) => {
-    const currentYearEvents = useSelector((state) => state.events.currentYear);
-    const dispatch = useDispatch();
-    const [eventName, setEventName] = useState(entryEvent ? entryEvent : "");
+	const currentYearEvents = useSelector((state) => state.events.currentYear);
+	const dispatch = useDispatch();
+	const [eventName, setEventName] = useState(entryEvent ? entryEvent : "");
 
-    useEffect(() => {
-        dispatch(fetchEvents());
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchEvents());
+	}, [dispatch]);
 
-    const handleChange = (e) => {
-        setEventName(e.target.value);
-    };
+	const handleChange = (e) => {
+		setEventName(e.target.value);
+	};
 
-    return (
-        <div className="dropdown" id="dropdown_event">
-            <label htmlFor="dropdown">Events:</label>
-            <select value={eventName} onChange={handleChange} name="event">
-                <option value="" disabled>
-                    Select Event
-                </option>
-                {currentYearEvents.map((event, index) => (
-                    <option value={event.name} key={index}>
-                        {event.name}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+	const eventsWithFullSeason = [
+		{
+			name: "FULL SEASON ENTRY",
+			date: "Oct 11",
+			startDate: "11/3/2025",
+			year: 2025,
+		},
+		...currentYearEvents,
+	];
+
+	return (
+		<div className="dropdown" id="dropdown_event">
+			<label htmlFor="dropdown">Events:</label>
+			<select value={eventName} onChange={handleChange} name="event">
+				<option value="" disabled>
+					Select Event
+				</option>
+				{eventsWithFullSeason.map((event, index) => (
+					<option value={event.name} key={index}>
+						{event.name}
+					</option>
+				))}
+			</select>
+		</div>
+	);
 };
 
 export default EventSelect;
