a477978b0c31f79fcf685c5ab0d0975e9d11a041
Brendan Woo <woo.brendan@gmail.com>
2026-03-13 10:36:02 -0500

    update starting state to be year 2026

--- podium-helper/src/components/EventsSearch.js
+++ podium-helper/src/components/EventsSearch.js
@@ -26,7 +26,7 @@ const EventSearch = ({ component, getValue }) => {
 				name: "FULL SEASON ENTRY",
 				date: "Oct 11",
 				startDate: "11/3/2025",
-				year: 2025,
+				year: 2026,
 			},
 			...currentYearEvents,
 		];
